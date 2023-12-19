ed2ffc2722f7d8992ec407be5cff1b5819056c00
ewerton-andrade <etonandrade@gmail.com>
2023-12-19 16:05:21 -0400

    Update index.tsx

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -3,11 +3,10 @@ import profileImg from "../../../public/Assets/perfil.jpg";
 const Card = () => {
   return (
     <div style={{ width: "10rem", height: "12.875rem", backgroundColor: "#fff", borderRadius: "1rem", margin: "0px", padding: "0px" }}>
-        <img src={profileImg} style={{ width: "100%", height: "10rem", borderRadius: "1rem 1rem 0rem 0rem", margin: "0px", border: "1px solid red", padding: "0px" }}/>
-        <div style={{ border: "1px solid red", width: "100%", height: "2.875rem", margin: "0px", padding: "0px" }}>
-          <p style={{ margin: "0px", padding: "0px" }}>Ewerton</p>
-        </div>
-
+      <img src={profileImg} style={{ width: "100%", borderRadius: "1rem 1rem 0rem 0rem", margin: "0px", border: "1px solid red", padding: "0px" }}/>
+      <div style={{ border: "1px solid red", width: "100%", height: "2.875rem", margin: "0px", padding: "0px" }}>
+        <p style={{ margin: "0px", padding: "0px" }}>Ewerton</p>
+      </div>
     </div>
   )
 }
