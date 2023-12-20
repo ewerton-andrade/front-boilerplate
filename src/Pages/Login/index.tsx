a703793c9473ba2b360c39791538ea4e091949f9
Diego Silva da Cunha <diego.cunha@itriad.org.br>
2023-12-20 16:06:00 -0400

    second

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -10,8 +10,9 @@ const Login = () => {
           <p style={{ fontSize: "3rem", fontFamily: "'Poppins', sans-serif", color: "#4287f5", fontWeight: "700", width: "100%", textAlign: "left" }}>facebook</p>
           <p style={{ fontFamily: "'Poppins', sans-serif", fontSize: "1.5rem", width: "100%", textAlign: "left", color: "#333" }}>Recent logins</p>
           <p style={{ fontFamily: "'Poppins', sans-serif", color: "#606770", fontSize: "1rem", textAlign: "left", width: "100%"}}>Click your picture or add an account.</p>
-          <div  style={{ width: "100%", height: "100%", display: "flex", alignItems: "center" }}>
-            <Card />
+          <div  style={{ width: "100%", height: "100%", display: "flex", alignItems: "center", gap: "1rem" }}>
+            <Card who="ewerton"/>
+            <Card who="Diego"/>
           </div>
         </div>
 
