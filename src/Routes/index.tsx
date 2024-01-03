0b1e785a9fcebb8a8441f1ac866be00a0fe69991
Diego Silva da Cunha <diego.cunha@itriad.org.br>
2024-01-03 18:53:49 -0400

    sidebar com react-router-dom

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import Login from "../Pages/Login";
 import Test from "../Pages/Test";
+import { PrivateRoutes } from "./privateRoutes";
+import PageTeste from "../Pages/PageTeste";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/test" element={<Test />} />
+        {/* <Route path="/test" element={<Test />} /> */}
+        <Route element={<PrivateRoutes />}>
+          <Route path="/test" element={<Test />}/>
+          <Route path="/pageteste" element={<PageTeste />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
